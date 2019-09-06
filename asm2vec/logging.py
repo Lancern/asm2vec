2c9156ba5f647ef0244c33b68efd2eb91a8dc907
Lancern <msrlancern@126.com>
2019-09-06 11:09:31 +0800

    Change default logging options.

--- asm2vec/logging.py
+++ asm2vec/logging.py
@@ -6,8 +6,8 @@ def asm2vec_logger() -> logging.Logger:
 
 
 def config_asm2vec_logging(**kwargs):
-    level = kwargs.get('level', logging.INFO)
-    handlers = kwargs.get('handlers', [logging.StreamHandler()])
+    level = kwargs.get('level', logging.WARNING)
+    handlers = kwargs.get('handlers', [])
     filters = kwargs.get('filters', [])
 
     asm2vec_logger().setLevel(level)
